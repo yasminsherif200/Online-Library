5f3ba0bb01540bf4ea578da643c12424edc3f2d1
yasmin416 <yasoohesam26@gmail.com>
2026-04-12 00:23:29 +0200

    edit

--- js/BookList.js
+++ js/BookList.js
@@ -15,9 +15,9 @@ document.querySelectorAll('.action-btn').forEach(btn => {
         const menu = document.createElement('div');
         menu.className = 'action-menu';
         menu.innerHTML = `
-            <a href="Edit-Book.html">✏️ Edit</a>
-            <a href="Books-management.html">🗑️ Delete</a>
-            <a href="BookDetails.html">👁️ View</a>
+            <a href="Edit-Book.html"><i class="fa-solid fa-pen"></i>Edit</a>
+            <a href="Books-management.html"><i class="fa-regular fa-trash-can"></i> Delete</a>
+            <a href="BookDetails.html"><i class="fa-solid fa-eye"></i> View</a>
         `;
         btn.style.position = 'relative';
         btn.appendChild(menu);
